bd2316c98145a7a45100c2025d15f3e604b9e954
Zach Harrison <zharrison100@gmail.com>
2021-02-02 02:16:27 +0000

    Deleted comment from exercise 4

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,13 +7,6 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
-# Surrey (annual_revenue of 224000, carries women's apparel only)
-# Whistler (annual_revenue of 1900000 carries men's apparel only)
-# Yaletown (annual_revenue of 430000 carries men's and women's apparel)
-# Using the where class method from Active Record, fetch (a collection of) only those stores that carry men's apparel. Assign the results to a variable @mens_stores.
-# Loop through each of these stores and output their name and annual revenue on each line.
-# Do another fetch but this time load stores that carry women's apparel and are generating less than $1M in annual revenue.
-
 Store.create( 
   name: "Surrey",
   annual_revenue: 224000,
